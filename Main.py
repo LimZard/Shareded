407af69b37f79dd2480738080ae3e4b684b5c8c3
LimZard <liinus.tillander@gmail.com>
2025-08-22 19:07:02 +0300

    Update Main.py

--- Main.py
+++ Main.py
@@ -1,6 +1,30 @@
+# Skibidi clock
+
+
 import streamlit as st
 from datetime import datetime
-from datetime import date
+from streamlit_autorefresh import st_autorefresh
+from datetime import time
+
+st_autorefresh(interval=1000, key="refresh")
+
+page_bg_img = """
+<style>
+[data-testid="stAppViewContainer"] {
+    background-image: url("https://wallpapercave.com/wp/AOW7Ii6.jpg");
+    background-size: cover;
+    background-position: center;
+    background-repeat: no-repeat;
+}
+[data-testid="stHeader"] {
+    background: rgba(0,0,0,0); /* transparent header */
+}
+[data-testid="stToolbar"] {
+    right: 2rem;
+}
+</style>
+"""
+st.markdown(page_bg_img, unsafe_allow_html=True)
 
 now = datetime.now()
 
@@ -8,17 +32,24 @@ year = now.year
 month = now.month
 day = now.day
 
-st.title("How long you live, yes?")
+finnish_hour = now.hour
+finnish_minute = now.minute
+finnish_second = now.second
 
-year_born = st.text_input("Enter Year of birth")
-month_born = st.text_input("Enter Month of birth")
-day_born = st.text_input("Enter Day of birth")
+finnish_time = time(finnish_hour, finnish_minute, finnish_second)
 
-if year_born != "" and month_born != "" and day_born != "":
-    today = date(year=year, month=month, day=day)
-    given_date = date(year=int(year_born), month=int(month_born), day=int(day_born))
-    diff = today - given_date
-    st.write(diff.days)
+with st.container():
+    st.write("---")
+    left, middle, right = st.columns(3)
+    with middle:
+        st.markdown(
+            f"<h8    style='font-size:64px;'>{finnish_time}</h1>",
+            unsafe_allow_html=True
+        )
+    with middle:
+        st.text(f"{day}/{month}/{year}")
+
+
 
 
 
