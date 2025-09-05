13ceea381dcd3ed42c8d778d00253cd826bcab08
LimZard <liinus.tillander@gmail.com>
2025-09-06 02:46:13 +0300

    Update Main.py

--- Main.py
+++ Main.py
@@ -1,3 +1,15 @@
+# Lista kahdesti tehtävä
+
+luku = int(input("Anna luku: "))
+
+while luku != 0:
+    original_list.append(luku)
+    print(f"Lista: {original_list}")
+    sorted_list = sorted(original_list)
+    print(f"Järjestettynä: {sorted_list}")
+    luku = int(input("Anna luku: "))
+print("Moi!")
+
 
 
 
