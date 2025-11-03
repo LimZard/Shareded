41b1f306e8ee70940a8e7de6b09f31f44f4c2730
LimZard <liinus.tillander@gmail.com>
2025-11-03 20:55:35 +0200

    Update Main.py

--- Main.py
+++ Main.py
@@ -1,6 +1,6 @@
 import streamlit as st
 docs_url = "https://docs.google.com/document/d/e/2PACX-1vS_JrejnKdNYYv4e3PpOuLqyGZ4KdUnitPI4w0xCKpe7LoyFw1KMe8-8aOvddWe8vur_aIDQ_rsGN_i/pub?embedded=true"
-st.set_page_config(page_title="Mantsan tutkielma", page_icon="🌍", layout="centered", layout="wide")
+st.set_page_config(page_title="Mantsan tutkielma", page_icon="🌍", layout="wide")
 st.components.v1.iframe(docs_url, width=999999, height=900000)
       
   
